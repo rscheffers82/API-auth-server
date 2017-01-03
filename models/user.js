76c66e6ae824bd92015f4718fab10bcb52c72136
Roy Scheffers <r.scheffers@gmail.com>
2017-01-03 12:14:14 -0600

    Add mongoose and a schema

---
+++ models/user.js
@@ -0,0 +1,25 @@
+// input to mongoose that describes the app
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define our model
+const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true
+  },
+  password: String
+});
+
+// NOTE: Mongoose does not deferentiate between upper and lower case so the below are the same
+// R.SCHEFFERS@GMAIL.COM
+// r.scheffers@gmail.com
+// to fix this add lowercase: true
+
+// Create the model class
+const ModelClass = mongoose.model('user, userSchema');
+
+// Export the model
+module.exports = ModelClass;
