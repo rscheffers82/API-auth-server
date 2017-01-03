7e0b8ebe63b4814cb12e52c8a74dd638b0f54e60
Roy Scheffers <r.scheffers@gmail.com>
2017-01-03 15:38:45 -0600

    Add bcrypt to hash users password when they register

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 // Define our model
 const userSchema = new Schema({
@@ -17,6 +18,29 @@ const userSchema = new Schema({
 // R.SCHEFFERS@GMAIL.COM
 // r.scheffers@gmail.com
 // to fix this add lowercase: true
+
+// On Save Hook, encrypt password
+
+// Before saving a model, run this function
+userSchema.pre('save', function (next) {
+  const user = this;      // access the user model, an instance of user
+
+  // generate a salt then run callback function
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) { return next(err); }
+
+    // hash (encrypt) our passowrd using the salt
+    // this takes time so run function when done.
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) { return next(err); }
+
+      // overwrite plain text password with encrypted password
+      user.password = hash;
+      next();
+    });
+  });
+});
+
 
 // Create the model class
 const ModelClass = mongoose.model('user', userSchema);
